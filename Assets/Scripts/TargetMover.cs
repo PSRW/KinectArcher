f2a232a1d872def762a247974204ccb133f8e07a
Jacek Byczkiewicz <jck53416@gmail.com>
2017-11-03 22:09:34 +0100

    Add background and moving target
    -Add new assets for background building
    -Remove unused experimental assets
    -Create background
    -Add new script responsible for moving the target

---
+++ Assets/Scripts/TargetMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMover : MonoBehaviour {
+
+    private Rigidbody2D targetBody;
+	// Use this for initialization
+	void Start () {
+        targetBody = GetComponent<Rigidbody2D>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        targetBody.velocity *= -1;
+    }
+}
